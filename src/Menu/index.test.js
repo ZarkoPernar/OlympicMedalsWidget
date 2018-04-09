003e3033ba8f2cf5a243b939e62c01658b3fe7f7
zarko.pernar <zarko.pernar@gmail.com>
2018-04-09 17:57:33 +0200

    Add test for Menu component

--- src/Menu/index.test.js
+++ src/Menu/index.test.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import Menu from './index'
+import MenuButton from './MenuButton'
 
 describe('Menu', () => {
-  it('should have a corresponding color according to name', () => {})
+  describe('when activeSortOption prop passed is "gold"', () => {
+    it('should have the gold Menu button isActive prop set to true', () => {
+      const wrapper = shallow(<Menu activeSortOption="gold" />)
+      const firstButton = wrapper.find(MenuButton).first()
+
+      expect(firstButton.props().isActive).toBe(true)
+    })
+  })
 })
